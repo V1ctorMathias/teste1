3d7b95df7dd342bd9190e75e4159f1e66302dea1
Victor Mathias <victormpporto@gmail.com>
2024-05-13 23:11:04 -0300

    Adicionado Modal e Lista de Compras

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 // REFERENTE AO CONSUMO
 const url = "https://api-stg-catalogo.redeancora.com.br/superbusca/api/integracao/catalogo/v2/produtos/query/sumario";
-const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkRFQkJDQUJBMjIwQjRGOTVDOTA5NTNFMURBMTlENEUzQzFDRDFGRDciLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiIzcnZLdWlJTFQ1WEpDVlBoMmhuVTQ4SE5IOWMifQ.eyJuYmYiOjE3MTU1MjgyOTAsImV4cCI6MTcxNTYxNDY5MCwiaXNzIjoiaHR0cHM6Ly9zc28tY2F0YWxvZ28ucmVkZWFuY29yYS5jb20uYnIiLCJhdWQiOiJTZWFyY2hFbmdpbmVBcGkuc3RnIiwiY2xpZW50X2lkIjoiNjV0dmg2cnZuNGQ3dWVyM2hxcW0ycDhrMnB2bm01d3giLCJyb2xlIjoicmVhZCIsInNjb3BlIjpbInNlYXJjaGVuZ2luZWFwaS5zdGciXX0.AXyBqG5VL6wbYVpunASsTbabHrguXTq6kf6HsD97Wfp6t-2TLPRYuvjkA81LAoc-T98YgToGwGTBUWPZ7lckuCXct_ZxCfh8GYLqcHX4HG-08Qzu3BfJlth-CL-16ac1VRmF82sGZMqch82bOG5MfBoyW_NexuAhtLjX5MhUgY2sUFurYn1H0qaxCvePRdvujfI-aQ1XyOqsMJTXhvFDwNZIfHO5ejdK-J5XWKYBfRFeMo8pnWUXGnHO3TkN2cXdlTdK55AcO_qVnpoIlcCv_D603a67fkxWtHj1KAG1ZjCp6aoa7r4hzKbq7yMEsPTrQ5njCiAqD6AGKjw3b1Rb2PdC0Pa8xgGZDGYv5Dg5GkQaDP_Jsq3UeR45TL7eieCnqU6jSuXdT5IeL8E5op1nncn_Sf8WMg2MnDEMB0xNQBIM--YHWmont68fhiuQ50MhTBRme8SUN_MXBniKJFe20KcvUvdLb_6VX41MOPeOlDyEd7ALA_H_yEZM9AGFmqO6fa-rEINs2DW8pSOCiYWKC21ZZRPox4k6cVKheuZ3tC8-nlG_INcFLLNiCJOPeDxrRA73lOyruINA2WA2aKVswG3lgLhyiIS8ZrvjFh0qWzt-NN1zwku8Movrz2322OK4emrNqJWWUdMT2fxyYfcd08FmCHOEVhnSZbSVRUnbnr8";
+let token = "";
 const tokenType = "Bearer";
+
+// URL e corpo da requisição de login
+const loginUrl = 'https://sso-catalogo.redeancora.com.br/connect/token';
+const loginData = {
+    client_id: '65tvh6rvn4d7uer3hqqm2p8k2pvnm5wx',
+    client_secret: '9Gt2dBRFTUgunSeRPqEFxwNgAfjNUPLP5EBvXKCn',
+    grant_type: 'client_credentials'
+};
+
+// Configurações da requisição
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(loginData)
+};
+
+// Função para fazer a requisição de login e retornar o token
+async function fetchToken() {
+    try {
+        const response = await fetch(loginUrl, requestOptions);
+        const data = await response.json();
+        token = data['access_token'];
+        return token 
+    } catch (error) {
+        console.error('Erro ao obter token:', error);
+        return null;
+    }
+}
 
 // CAPTURA DOS OBJETOS DO HTML
 const formulario = document.querySelector('#formulario');
 const inputBuscar = document.querySelector('#inputBuscar');
-const listaOpcoes = document.querySelector('.produtos')
+const listaOpcoes = document.querySelector('.produtos');
+const carrinho = document.querySelector('.carrinho');
+const modalCarrinho = document.querySelector('.modalCarrinho');
 
 let precoOriginal = 0;
 let promocao = 0;
+let precoPromocao = 0;
 
 lstOpcoes = [];
 lstCarrinho = [];
@@ -17,6 +50,7 @@ lstCarrinho = [];
 // CONSULTA DA API
 async function buscarProduto(pesquisa) {
     try {
+        await fetchToken();
         const response = await fetch(url, {
             method: "POST",
             headers: {
@@ -64,9 +98,11 @@ function mostrarOpcoes() {
                                 <p class="spanNome">${i['nomeProduto']}</p>
                                 <p class="spanMarca">por ${i['marca']}</p>
                             </div>
-                            <span class="spanPrecoAnterior">R$ ${precoOriginal = geraValorPromocao()}.00</span>
-                            <span class="spanPreco">R$ ${(precoOriginal - (precoOriginal * ( (promocao = geraPromocaoAleatorio()) / 100 ) )).toFixed(2)}  <span style="color: #00a650">${promocao}% OFF</span></span>
-                            <button onclick="adicionarCarrinho(${i['id']})">Comprar</button>
+                            <div class="spanPrecos">
+                                <p class="spanPrecoAnterior">R$ ${precoOriginal = geraValorPromocao()}</p>
+                                <p class="spanPreco">R$ ${precoPromocao = (precoOriginal - (precoOriginal * ( (promocao = geraPromocaoAleatorio()) / 100 ) )).toFixed(2)}  <span style="color: #00a650">${promocao}% OFF</span></p>
+                            </div>
+                            <button onclick="adicionarCarrinho(${i['id']}, ${precoPromocao})">Comprar</button>
                         </div>
                     </div>
                  </div>`;
@@ -84,7 +120,7 @@ formulario.addEventListener('submit', (e) => {
 
 // Funcao de precos aleatorios
 function geraValorPromocao() {
-    return Math.floor(Math.random() * (300 - 10 + 1)) + 10;
+    return (Math.random() * (300 - 10 + 1) + 10).toFixed(2);
 }
 
 function geraPromocaoAleatorio() {
@@ -92,7 +128,27 @@ function geraPromocaoAleatorio() {
 }
 
 // Funcao que adiciona o produto ao carrinho a partir de seu ID
-function adicionarCarrinho(id) {
-    lstCarrinho = lstOpcoes.filter(item => (item['id'] == id));
-    console.log(lstCarrinho[0])
+function adicionarCarrinho(id, valor) {
+    let itemCarrinho = lstOpcoes.filter(item => (item['id'] == id))[0];
+    lstCarrinho.push({
+        id: itemCarrinho['id'],
+        nome: itemCarrinho['nomeProduto'],
+        preco: valor
+    })
+    console.log(lstCarrinho)
 }
+
+// Aparecer Modal
+carrinho.addEventListener('click', function () {
+    debugger
+    modalCarrinho.innerHTML = '';
+    for (let item of lstCarrinho) {
+        modalCarrinho.innerHTML += 
+        `
+        <p>${item['id']}</p>
+        <p>${item['nome']}</p>
+        <p>${item['preco']}</p>
+        `
+    }
+    modal.showModal();
+})
